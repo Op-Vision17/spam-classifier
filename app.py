7f9adb51fccaf4d2acd49ec35f42a650bee272cb
Arjun Gupta <guptaarjun1711@gmail.com>
2025-07-02 19:45:17 +0530

    nltk files

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import streamlit as st
 import pickle
 import string
-# Text preprocessing
-from nltk.corpus import stopwords
 import nltk
+nltk.data.path.append('nltk_data')
+from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
-
 
 def transform_text(text):
     text = text.lower()
@@ -43,13 +42,9 @@ st.title("Email/SMS Spam Classifier")
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
-    # 1. Preprocess
     transformed_sms = transform_text(input_sms)
-    # 2. Vectorize
     vector_input = tfidf.transform([transformed_sms])
-    # 3. Predict
     result = model.predict(vector_input)[0]
-    # 4. Display
     if result == 1:
         st.header("Spam")
     else:
